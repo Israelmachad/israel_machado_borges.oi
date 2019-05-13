cbe82f1bd644d4b92e95fb2d249c409d4f17a320
italo regis dos santos <sraelmachado@gmail.com>
2019-05-13 17:22:05 -0300

    config

--- peladas/src/br/com/peladas/controller/PeladasController.java
+++ peladas/src/br/com/peladas/controller/PeladasController.java
@@ -1,5 +1,17 @@
 package br.com.peladas.controller;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "peladas")
 public class PeladasController {
 
+	private String value = "teste";
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
